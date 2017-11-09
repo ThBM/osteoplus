964909af1389df63d28643a0e6ebbdfe6ecd577a
ThBM <thibault.bm@icloud.com>
2017-11-09 13:41:32 +0100

    Add .active class to relevant menu for each view

--- models/seance.js
+++ models/seance.js
@@ -5,7 +5,8 @@ const mongoose = require("../config/mongoose")
 let seanceSchema = mongoose.Schema({
   patient    : {type: mongoose.Schema.Types.ObjectId, ref: "Patient", require: true},
   startTime  : {type: "Moment", require: true},
-  endTime    : {type: "Moment", require: true}
+  endTime    : {type: "Moment", require: true},
+  comments   : {type: String, require: true}
 })
 
 seanceSchema.statics.findForPatient = function(patientId, query, callback) {
